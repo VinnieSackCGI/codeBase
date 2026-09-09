8c0176f72e742a0181b468572f35b60ac33a2956
Vinson Sack <vinson.sack@cgifederal.com>
2026-09-09 14:23:59 -0400

    Add files via upload

---
+++ EXConnect Plugins/Configuration/ConfigurationData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Plugins.Configuration
+{
+	public static class Data
+	{
+		public static string PortalName
+		{
+			get => "EXConnect";
+		}
+	}
+}
